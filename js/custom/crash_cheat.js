0f2de23debabaa3a7b31221bf75855e6b186be23
linzeliang1222 <linzeliang1222@gmail.com>
2021-04-13 00:33:04 +0800

    Site updated: 2021-04-13 00:33:03

--- js/custom/crash_cheat.js
+++ js/custom/crash_cheat.js
@@ -1,16 +1,1 @@
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    $('[rel="icon"]').attr('href', "/joke.ico");
-    document.title = '这里这里 ◕ ں ◕ ';
-    clearTimeout(titleTime);
-  }
-  else {
-    $('[rel="icon"]').attr('href', "/favicon.ico");
-    document.title = '(ฅ>ω<*ฅ) 喵喵爱你哟~' + OriginTitle;
-    titleTime = setTimeout(function () {
-	    document.title = OriginTitle;
-    }, 2000);
-  }
-});
+"use strict";var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/joke.ico"),document.title="这里这里 ◕ ں ◕ ",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="(ฅ>ω<*ฅ) 喵喵爱你哟~"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
